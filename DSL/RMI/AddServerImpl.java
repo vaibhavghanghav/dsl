594b9dec877d3934228b4e7c6f817c9151785f47
Vaibhav Ghanghav <86397409+vaibhavghanghav@users.noreply.github.com>
2024-12-04 21:48:56 +0530

    Delete DSL directory

--- DSL/RMI/AddServerImpl.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-import java.rmi.server.*;
-
-public class AddServerImpl extends UnicastRemoteObject implements AddServerIntf {
-    public AddServerImpl() throws RemoteException {}
-    public double add(double num1, double num2) throws RemoteException {
-        return num1 + num2;
-    }
-}
--- DSL/RMI/AddServerIntf.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.*;
-
-public interface AddServerIntf extends Remote {
-    double add(double num1, double num2) throws RemoteException;
-}
